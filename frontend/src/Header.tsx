9d35b286abd9b0eff7e38209b1a9f5d780ce396b
Alexander Maat <amaat@byu.edu>
2024-03-18 14:35:54 -0600

    video 4

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,7 +1,16 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 
-function Header() {
-  return <></>;
+function Header(props: any) {
+  return (
+    <header className="row navbar navbar-dark bg-dark">
+      <div className="col-4">
+        <img src={logo} className="logo" alt="logo" height={100} />
+      </div>
+      <div className="col subtitle">
+        <h1 className="text-white">{props.title}</h1>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
